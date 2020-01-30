0bf8c8106371badab41d2455037216c3afea678e
glopez0152@richmondhillhs.org <https://github.com/Team6593/frc2020test.git>
2020-01-30 17:26:45 -0500

    First commit

--- src/main/java/frc/robot/commands/piston_Stop.java
+++ src/main/java/frc/robot/commands/piston_Stop.java
@@ -8,12 +8,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 
 public class piston_Stop extends CommandBase {
   /**
    * Creates a new piston_Stop.
    */
   public piston_Stop() {
+    addRequirements(Robot.pistons);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -25,11 +27,13 @@ public class piston_Stop extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    Robot.pistons.stop();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    Robot.pistons.stop();
   }
 
   // Returns true when the command should end.
